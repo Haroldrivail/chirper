ed7176564672c78dd2f7b390c778629c35ebea99
Harold <dongrold@gmail.com>
2025-09-20 17:01:46 +0100

    feat: implement DaisyUI styling for chirp form and layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,43 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChirpController::class, 'index']);
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::controller(ChirpController::class)->group(function () {
+        Route::post('/chirps', 'store');
+        Route::get('/chirps/{chirp}/edit', 'edit');
+        Route::put('/chirps/{chirp}', 'update');
+        Route::delete('/chirps/{chirp}', 'destroy');
+    });
 });
+
+// Route::resource('/chirps', ChirpController::class)
+//     ->only(['index, store, edit, update, delete']);
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+// Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
